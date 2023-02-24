ea86a3c38c3110038e3e40b0551e4c4a10a61425
OlyaChuba <chubaolya33@gmail.com>
2023-02-24 19:18:05 +0200

    first commit

---
+++ js/start.js
@@ -0,0 +1,43 @@
+// it`s a comments
+//in-line comment
+/* multi-line comment
+i can write in another line */
+
+// DATA TYPES AND VARIABLES
+/* Data Types: there are 7 types:
+1. underfined - smt hasn`t been defined
+2. null - nothing
+3. boolen - True or Falce
+4. symbol - is immutable primitive value
+5. number - number
+6. string - words
+7. object
+
+var - variable "змінна" */
+
+//There are 3 types to declare a variable:
+var myName = "Olya"
+myName = 21
+let ourName = 'Olya'
+const pi = 3.14  // const never change
+
+
+
+
+
+//STORING VALUES WITH ASSIGNMENT OPERATOR - ЗБЕРЕЖЕННЯ ЗНАЧЕНЬ З ОПЕРАЦІЄЮ ПРИСВОЄННЯ
+
+var a;  //asign a variable    "semicolon" - крапка з комою
+var b = 2; //declaring a variable
+
+a = 4
+
+console.log(a);
+
+
+
+
+//OPERATORS: adding, 
+
+var sum = 10+10;
+console.log(sum)
