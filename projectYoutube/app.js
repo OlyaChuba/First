f9af8a41c0237b850f853bb27e19fffa81653bb4
OlyaChuba <chubaolya33@gmail.com>
2023-02-26 20:39:17 +0200

    woringProgress

--- projectYoutube/app.js
+++ projectYoutube/app.js
@@ -1,3 +1,40 @@
+
+//Selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo)
+
+
+//Functions
+function addTodo(event){
+    alert("test1")
+    //Prevent form from submitting
+    event.preventDefault();
+    
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create Li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "hey";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashdButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+
+   
+
+
+}
